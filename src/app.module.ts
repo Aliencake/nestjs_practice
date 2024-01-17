32ebdeeaf8eaacccc3185d3fa49b232a37a23996
Aliencake <voloshko30@gmail.com>
2024-01-17 19:52:54 +0200

    add users and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    database: process.env.DATABASE_NAME,
+    port: 32768,
+    host: process.env.DATABASE_HOST,
+    username: process.env.DATABASE_USERNAME,
+    password: String(process.env.DATABASE_PASSWORD),
+    entities: [],
+  }),
+  UsersModule,
+  AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
